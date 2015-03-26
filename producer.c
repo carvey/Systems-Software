5ffa6a93a457f6167f952afedaa82afa6596fe7a
charles <j95.charles@gmail.com>
2015-03-26 00:00:47 -0400

    The producer/consumer code is documented and should be working. And the pthread.c file has been updated just a little, but could use some more tinkering.

--- producer.c
+++ producer.c
@@ -1,40 +1,79 @@
+/*
+This file should create 2 threads which produce and create cupcakes in unison.
+10 items should be produced, and consumers shold consume all 10 of these items
+The produce should sleep 2 seconds after each produced product, and consumers should visit at random
+*/
+
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void produce(int count);
-void consume(int count);
+void *produce(void *count);
+void *consume(void *count);
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-COUNT_MAX = 10;
+int COUNT_MAX = 10;
+
 
 int main()
 {
+	//set a seed for the random number generator
+	srand(time(NULL));
 
-int count = 0;
+	int count = 0;
 
-pthread_t produce_thread;
-pthread_t consume_thread;
+	//Init the two threads
+	pthread_t produce_thread;
+	pthread_t consume_thread;
 
-pthread_create(&produce_thread, NULL, produce, count);
+	//start each thread with its respective function as a start routine and pass it the memory address of count to mess with
+	pthread_create(&produce_thread, NULL, produce, &count);
+	pthread_create(&consume_thread, NULL, consume, &count);
 
-pthread_create(&consume_thread, NULL, consume, count);
 
-
-}
-
-void produce(int count)
-{
-
-	if (++count <= COUNT_MAX)
-	{
-		
+	if (pthread_join(consume_thread, NULL)) {
+		fprintf(stderr, "Error joining thread\n");
+		return 2;
 	}
-
+	printf("Final count (for confirmation): %d\n", count);
 }
 
-
-void consume(int count)
+//This function will increment count up to 10 with 2 seconds inbetween each increment
+void *produce(void *counter)
 {
+	//this is cast to an int pointer the pthread_create needs to be have a void function and the arg needs to void
+	int *count = (int *)counter;
+	int i;
+	for (i = 0; i < COUNT_MAX; ++i)
+	{
+		//increment count
+		*count = ++(*count);
+		printf("Produced! Count is now: %d\n", *count);
+		sleep(2);
+	}
+	return NULL;
+}
 
-
+//This function will decrement count to 0 with random intervals (between 0-6 seconds)
+//If the count is 0, the loop will keep checking until the count is not 0. Then it will sleep -> decrement -> print
+void *consume(void *counter)
+{
+	int *count = (int *)counter;
+	int i; 
+	for (i=0; i < COUNT_MAX; i++)
+	{
+		if (*count == 0)
+		{
+			i--;
+			continue;
+		}
+		else
+		{
+			//sleep a random num of second between 0 and 6
+			sleep(rand() % 7);
+			//decrement count
+			*count = --(*count);
+			printf("Consumed! Count is now: %d\n", *count);
+		}
+	}
 
 }
--- pthread.c
+++ pthread.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void *inc_x(void *x_void_ptr);
+void *inc_y(void *y_void_ptr);
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
 int main()
@@ -12,22 +13,15 @@ int main()
 	printf("x: %d, y: %d\n", x, y);
 
 	pthread_mutex_lock(&m);
+
 	pthread_t inc_x_thread;
+	pthread_t inc_y_thread;
 
-	if (pthread_create(&inc_x_thread, NULL, inc_x, &x))
-	{
-		fprintf(stderr, "Error creating thread \n");
-		return 1;
-	}
+	pthread_create(&inc_x_thread, NULL, inc_x, &x);
+	// pthread_create(&inc_y_thread, NULL, inc_y, &y);
 
-	pthread_t inc_y;
-	pthread_create(&inc_y, NULL, inc_x, &y);
+	inc_y(&y);
 
-	// pthread_mutex_lock(&m);
-	while (++y < 10)
-	{
-		printf("current val for y is: %d using thread %ld \n", y, pthread_self());
-	}
 	pthread_mutex_unlock(&m);
 
 	printf("y increment finished\n");
@@ -57,18 +51,16 @@ void *inc_x(void *x_void_ptr)
 	return NULL;
 }
 
-// void *inc_y(void *y_void_ptr)
-// {
-// 	// pthread_mutex_lock(&m);
+void *inc_y(void *y_void_ptr)
+{
 
-// 	int *y_ptr = (int *)y_void_ptr;
-// 	while(++(*x_ptr) < 10)
-// 	{
-// 		printf("current val for x is %d, incremented by %ld\n", *y_ptr, pthread_self());
-// 	}
-// 	printf("y increment finished \n");
+	int *y_ptr = (int *)y_void_ptr;
+	while(++(*y_ptr) < 10)
+	{
+		printf("current val for y is %d, incremented by %ld\n", *y_ptr, pthread_self());
+	}
+	printf("y increment finished \n");
 
-// 	pthread_mutex_unlock(&m);
-// 	return NULL;
+	return NULL;
 
-// }
+}
