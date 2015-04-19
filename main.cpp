e98093867a50010a241d33dc46f3c459d3ed17c7
charles <j95.charles@gmail.com>
2015-04-19 17:08:25 -0400

    ready to implement algorithms

--- main.cpp
+++ main.cpp
@@ -7,19 +7,18 @@ int main()
 {
 	srand(time(NULL));
 
-	// memory of 10 pages with set Page numbers
+	// memory of 10 pages and 2 segments with set Page numbers
 	vector<int> pages_array = {1, 2, 2, 3, 7, 5, 3, 2, 9, 4};
 	Memory mem(pages_array, 2);
 	mem.print_pages();
 
-	// Memory of 10 pages with random numbers
+	// Memory of 15 pages and 5 segments with random numbers
 	Memory mem2(15, 5);
 	mem2.print_pages();
 
 	//The print call on a memory instance should display a full table
 	cout << endl;
 	mem2.print();
-
 
 	return 0;
 }
--- memory.cpp
+++ memory.cpp
@@ -7,97 +7,168 @@ using namespace std;
 
 class Page
 {
-	public:
-		int number;
+public:
+    int number;
 
-		Page()
-		{
-			number = rand() % 10;
-		}
+    Page()
+    {
+        number = rand() % 10;
+    }
 
-		Page(int num)
-		{
-			number = num;
-		};
+    Page(int num)
+    {
+        number = num;
+    };
+
+    char print()
+    {
+        if (number == -1)
+        {
+            return ' ';
+        }
+        else
+        {
+            return (char)number;
+        }
+    }
 
 };
 
 class Memory
 {
-	public:
-		vector<Page> pages;
-		int segments;
+private:
+    vector<Page> y_vector;
+    vector<Page> x_vector;
 
-		Memory(int size = 10, int memory_segments=3)
-		{
-			segments = memory_segments;
+public:
+    vector<Page> pages;
+    vector<vector<Page> > ps_mapping;
+    int segments;
 
-			for (int i = 0; i < size; i++)
-			{
-				pages.push_back(Page());
-			}
-		};
+    Memory(int size = 10, int memory_segments=3)
+    {
+        segments = memory_segments;
 
-		Memory(vector<int> pages_vector, int memory_segments=3)
-		{
-			segments = memory_segments;
-			
-			for (int i = 0; i < pages_vector.size(); i++)
-			{
-				pages.push_back(
-					Page(pages_vector.at(i))
-					);
-			}
-		};
+        for (int i = 0; i < size; i++)
+        {
+            pages.push_back(Page());
+        }
 
-		void print_pages()
-		{
-			cout << "[";
-			for (int i=0; i<pages.size(); i++)
-			{
-				cout << pages.at(i).number;
-				if (i != pages.size()-1)
-					cout << ", ";
-				else
-				{
-					cout << "]";
-					cout << endl;
-				}
-			}
-		};
+        for (int i=0; i<size; i++)
+        {
+            vector<Page> row(segments, Page(-1));
+            ps_mapping.push_back(row);
+        }
+    };
 
-		void print()
-		{
-			for (int i = 0; i < pages.size(); i++)
-			{
-				cout << "  " << pages.at(i).number << " ";
-			}
+    Memory(vector<int> pages_vector, int memory_segments=3)
+    {
+        segments = memory_segments;
 
-			cout << endl;
-			
-			for (int i = 0; i < segments; i++)
-			{
-				cout << "-";
-				for (int i = 0; i < pages.size(); i++)
-				{
-					cout << "----";
-				}
-				cout << endl;
 
-				for (int k=0; k < pages.size(); k++)
-				{
-					cout << "|   ";
-				}
-				cout << "|";
-				cout << endl;
-			}
+        for (int i = 0; i < pages_vector.size(); i++)
+        {
+            pages.push_back(
+                    Page(pages_vector.at(i))
+            );
+        }
 
-			cout << "-";
-			for (int i = 0; i < pages.size(); i++)
-			{
-				cout << "----";
-			}
-			cout << endl;
+        int size = pages.size();
+        for (int i=0; i<size; i++)
+        {
+            vector<Page> row(segments, Page(-1));
+            ps_mapping.push_back(row);
+        }
 
-		};
+    };
+
+    void print_pages()
+    {
+        cout << "[";
+        for (int i=0; i<pages.size(); i++)
+        {
+            cout << pages.at(i).number;
+            if (i != pages.size()-1)
+                cout << ", ";
+            else
+            {
+                cout << "]";
+                cout << endl;
+            }
+        }
+    };
+
+    void print()
+    {
+        for (int i = 0; i < pages.size(); i++)
+        {
+            cout << "  " << pages.at(i).number << " ";
+        }
+
+        cout << endl;
+
+        for (int i = 0; i < segments; i++)
+        {
+            cout << "-";
+            for (int j = 0; j < pages.size(); j++)
+            {
+                cout << "----";
+            }
+            cout << endl;
+
+            for (int k=0; k < pages.size(); k++)
+            {
+                cout << "| " << ps_mapping[k][i].print() << " ";
+            }
+            cout << "|";
+            cout << endl;
+        }
+
+        cout << "-";
+        for (int i = 0; i < pages.size(); i++)
+        {
+            cout << "----";
+        }
+        cout << endl;
+
+    };
+
+    vector<double> play_fifo()
+    {
+        double time = 0;
+        double hits = 0;
+        double faults = 0;
+
+        //implement fifo here
+
+        vector<double> final = {time, hits, faults};
+        return final;
+    };
+
+    vector<double> play_lru()
+    {
+        double time = 0;
+        double hits = 0;
+        double faults = 0;
+
+        //implement lru here
+
+        vector<double> final = {time, hits, faults};
+        return final;
+    };
+
+    vector<double> play_optimal()
+    {
+
+        double time = 0;
+        double hits = 0;
+        double faults = 0;
+
+        //implement optimal here
+
+        vector<double> final = {time, hits, faults};
+        return final;
+    };
+
+
 };
